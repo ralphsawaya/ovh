23c96a6f17c55ed02fc563a479607e4e2bb01c04
ralphsawaya <ralph.sawaya@gmail.com>
2024-07-19 15:53:59 +0200

    Create variables.tf

---
+++ Terraform/variables.tf
@@ -0,0 +1,80 @@
+variable "ovh_application_key" {
+  description = "OVH application key"
+  type        = string
+}
+
+variable "ovh_application_secret" {
+  description = "OVH application secret"
+  type        = string
+}
+
+variable "ovh_consumer_key" {
+  description = "OVH consumer key"
+  type        = string
+}
+
+variable "ovh_project_id" {
+  description = "OVH Project ID"
+  type        = string
+}
+
+variable "ovh_s3_endpoint" {
+  description = "OVH S3 endpoint"
+  type        = string
+}
+
+variable "ovh_s3_bucket_endpoint" {
+  description = "OVH S3 bucket endpoint"
+  type        = string
+}
+
+variable "ovh_s3_bucket_name" {
+  description = "OVH S3 bucket name"
+  type        = string
+}
+
+variable "ovh_s3_access_key" {
+  description = "OVH S3 access key"
+  type        = string
+}
+
+variable "ovh_s3_secret_key" {
+  description = "OVH S3 secret key"
+  type        = string
+}
+
+variable "ovh_s3_user_id" {
+  description = "OVH S3 secret key"
+  type        = string
+}
+
+variable "auth_url" {
+  description = "OpenStack authentication URL"
+  type        = string
+}
+
+variable "domain_name" {
+  description = "OpenStack domain name"
+  type        = string
+}
+
+variable "region" {
+  description = "OpenStack region"
+  type        = string
+}
+
+variable "image_name" {
+  description = "Name of the image to use for the VM"
+  type        = string
+}
+
+variable "flavor_name" {
+  description = "Name of the flavor to use for the VM"
+  type        = string
+}
+
+variable "key_pair_name" {
+  description = "Name of the SSH key pair to use"
+  type        = string
+  default     = "ssh_keypair"
+}
